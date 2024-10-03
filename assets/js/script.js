292a4d70ae565df06a40fa9c91d7eb0cb0ec79a8
amirabourogaa <abouragba@u-label.com>
2024-10-03 12:03:08 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,234 @@
+const translations = {
+  fr: {
+    websiteTitle: '🌿Ghaba🌿',
+    heroTitle: 'Bienvenue à Jardin Nature',
+    heroDescription: "14 ans au service de l'agriculture.",
+    menu: {
+      home: 'Accueil',
+      products: 'Produits',
+      about: 'Qui sommes nous?',
+      contact: 'Contact'
+    },
+    productSection: {
+      title: 'Nos produits',
+      description: `
+Chez Ghaba.tn, nous vous offrons une large gamme de produits naturels liés à la forêt, conçus pour répondre à vos besoins en jardinage et en aménagement extérieur. Nos produits incluent :
+
+Plantes et Fleurs : Une variété de plantes naturelles et de fleurs pour ajouter une touche de verdure à votre espace.
+Accessoires de Jardinage : Des outils et accessoires pour vous aider à entretenir facilement votre jardin.
+Produits Organiques : Engrais et autres produits naturels pour une terre fertile et des plantes en bonne santé.
+Bois et Produits de Forêt : Des matériaux en bois pour divers usages, allant de l'aménagement au bricolage.
+      `,
+      categories: {
+        all: 'Tous',
+        cat1: 'Pollinisateurs',
+        cat2: 'Équipements agricoles manuels'
+      },
+      products: {
+        prod1: `Pollinisateur mécanique télescopique électrique (ou par groupe électrogène 500w) pour palmier dattier.
+        90 dattiers par heure, hauteur 10m sans grimper.`,
+        prod2: `Pollinisateur mécanique télescopique à batterie intégrée pour palmier dattier.
+        90 dattiers par heure, hauteur 10m sans grimper.`,
+        prod3: `Pollinisateur télescopique manuel pour palmier dattier et pistachier.
+        Hauteur jusqu’à 5m sans grimper.`,
+        prod4: `Transplanteur manuel pour légumes.
+        Planter facilement et efficacement en position debout.`,
+        prod5: `Semoir manuel pour diverses graines.
+        Semer facilement et efficacement en position debout.`,
+        prod6: `Charrue manuelle avec six dents, profondeur 25cm.`,
+        prod7: `Désherbeur manuel en forme de « S », même dans les endroits difficiles.`,
+        prod8: `Désherbeur manuel plat, utilisable même dans les endroits difficiles.`,
+        prod9: `Unités et matériels de production de fourrage vers hors sol (hydroponique) pour l'alimentation des animaux d'élevage.`
+      },
+
+      actions: {
+        btn1: '',
+        btn2: ''
+      }
+    },
+    aboutSection: {
+      title: 'À propos de nous',
+      subtitle: `Notre entreprise a été créée en 2011 en tant que première entreprise en Afrique du Nord dans le secteur de la fabrication des Unités et matériels de production de fourrage vers hors sol « hydroponique » pour l'alimentation des animaux d’élevage.`,
+      description: `
+L'entreprise dispose d'un centre de formation spécialisé en agriculture hors-sol... (Hydroponique).
+La société Al-Ghaba fabrique également du matériel agricole tel que des machines de pollinisation mécaniques des palmiers.
+L'entreprise a réalisé plusieurs projets de production de fourrages verts hors-sol dans de nombreux pays tels que la Tunisie, l'Algérie, la Libye, le Koweït, le Qatar, la Palestine, l’Arabie Saoudite, le Mali, Djibouti...
+La société a également déjà coopéré avec l’Organisation des Nations Unies pour l'alimentation et l'agriculture.
+Nous apprécions la coopération avec ceux qui souhaitent être des distributeurs et représentants de notre entreprise dans leur pays.
+      `
+    },
+    contactSection: {
+      title: 'Contactez-nous',
+      description: `
+      Nous aimerions avoir de vos nouvelles ! Que vous ayez des questions sur nos produits, besoin d'informations supplémentaires, ou que vous souhaitiez partager votre expérience, notre équipe est à votre écoute. N'hésitez pas à nous contacter par téléphone, e-mail, ou via nos réseaux sociaux. Ensemble, créons un espace naturel qui vous ressemble.
+      `,
+      adressTitle: 'Addresse',
+      adressText: 'Omrane supérieur, Tunis',
+      telTitle: 'Téléphone',
+      emailtitle: 'Email'
+    },
+    footer: {
+      footerTitle: '🌿Ghaba🌿',
+      footerDesc: `Chez Ghaba.tn, nous nous engageons à apporter la nature plus près de vous avec des produits de qualité et respectueux de l'environnement. Suivez-nous sur nos réseaux sociaux pour rester informé de nos dernières nouveautés et conseils de jardinage.`,
+      footerText: '© Tous droits réservés - Jardin Nature - 2024'
+    }
+  },
+  ar: {
+    websiteTitle: '🌿الغابة🌿',
+    heroTitle: 'مرحبًا بكم في جاردين ناتور',
+    heroDescription: `شركة الغابة منذ 2011 نحن في خدمة وتطوير الفلاحة`,
+    menu: {
+      home: 'الرئيسية',
+      products: 'منتجاتنا',
+      about: 'من نحن؟',
+      contact: 'اتصل بنا'
+    },
+    productSection: {
+      title: 'منتجاتنا',
+      description: `
+      في غابة.تن، نقدم مجموعة واسعة من المنتجات الطبيعية المتعلقة بالغابة، المصممة لتلبية احتياجاتكم في الزراعة والبستنة. تشمل منتجاتنا ما يلي:
+
+النباتات والزهور: مجموعة متنوعة من النباتات الطبيعية والزهور لإضفاء لمسة من الجمال على مساحتكم.
+إكسسوارات البستنة: أدوات وإكسسوارات تساعدكم في العناية بحديقتكم بكل سهولة وكفاءة.
+المنتجات العضوية: أسمدة ومنتجات طبيعية لضمان خصوبة التربة وصحة النباتات.
+الخشب ومنتجات الغابة: مواد خشبية طبيعية متعددة الاستخدامات لأغراض الزراعة أو الديكور.
+      `,
+      categories: {
+        all: 'جميع المنتجات',
+        cat1: 'الملقحات',
+        cat2: ' المعدات الزراعية اليدوية'
+      },
+      products: {
+        prod1: `ملقحة الية تلسكوبيه للنخيل
+تشتغل بالكهرباء أو بمولد كهربائي 500 وات، تلقيح قرابة 90 نخلة في الساعة لارتفاع يتجاوز 10 أمتار دون تسلق النخلة، وتستعمل أيضًا للمداواة.`,
+        prod2: `ملقحة ألية تلسكوبيه للنخيل ببطريه مدمجة
+تلقيح قرابة 90 نخلة في الساعة لارتفاع يقارب 10 أمتار دون تسلق النخلة، وتستعمل للمداواة.`,
+        prod3: `ملقحة يدوية تلسكوبيه للنخيل والفستق
+لارتفاع يقارب 5 أمتار دون تسلق النخلة، وتستعمل أيضًا للمداواة.
+`,
+        prod4: `آلة يدوية لزراعة مشاتل الخضر والبطاطا
+زراعة بسرعة وعمق مدروس دون الانحناء.
+`,
+        prod5: `
+آلة يدوية لزراعة مختلف أصناف البذور
+زراعة بسرعة وعمق مدروس دون الانحناء.`,
+        prod6: `محراث يدوي ومقلب تربة 6 أسنان عمق 25 سم`,
+        prod7: `أداة تحمير وإزالة الأعشاب حتى في الأماكن الصعبة`,
+        prod8: `أداة إزالة الأعشاب حتى في الأماكن الصعبة`,
+        prod9: `وحدات ومعدات إنتاج علف الشعير الأخضر المستنبت بدون تربة كعلف رخيص للحيوانات العاشبة`
+      },
+      actions: {
+        btn1: '',
+        btn2: ''
+      }
+    },
+    aboutSection: {
+      title: 'من نحن',
+      subtitle: `
+تأسست الشركة منذ 2011 كأول شركة عربية في شمال افريقيا تنشط في قطاع تصنيع وتركيب وحدات استنبات علف الشعير الأخضر بدون تربة (هيدروبونيك) كعلف طبيعي للحيوانات العاشبة، `,
+      description: `
+
+      إضافة الى مركز تدريب متخصص في الزراعة بدون تربة
+      كما تصنع شركة الغابة معدات فلاحية مثل الات التلقيح الميكانيكي للنخيل...
+      انجزت الشركة عدة مشاريع استنبات اعلاف خضراء بعديد الدول مثل تونس، الجزائر، ليبيا، الكويت فلسطين قطر، السعودية، مالي، دحيبوتي...
+      كما سبق لشركتنا التعاون مع منظمة الأغذية والزراعة للأمم المتحدة...
+      نرحب بالتعاون مع من يردون ان يكونوا وكلاء للشركة ببلدانهم...
+
+      `
+    },
+    contactSection: {
+      title: 'اتصل بنا',
+      description: `
+      نحن نتطلع لسماع أخباركم! سواء كانت لديكم أسئلة حول منتجاتنا، أو تحتاجون إلى معلومات إضافية، أو ترغبون في مشاركة تجربتكم، فإن فريقنا جاهز للاستماع. لا تترددوا في الاتصال بنا عبر الهاتف أو البريد الإلكتروني أو من خلال وسائل التواصل الاجتماعي الخاصة بنا. معًا، لنخلق مساحة طبيعية تناسب احتياجاتكم.
+ `,
+      adressTitle: 'العنوان',
+      adressText: 'عمران العلوي، تونس',
+      telTitle: 'الهاتف',
+      emailtitle: 'البريد الإلكتروني'
+    },
+    footer: {
+      footerTitle: '🌿الغابة🌿',
+      footerDesc: `في غابة.تن، نلتزم بتقريب الطبيعة إليكم من خلال منتجات عالية الجودة وصديقة للبيئة. تابعونا على وسائل التواصل الاجتماعي للبقاء على اطلاع بأحدث منتجاتنا ونصائح البستنة.
+`,
+      footerText: 'جميع الحقوق محفوظة - 🌿الغابة🌿 - 2024'
+    }
+  }
+}
+
+function changeLanguage (lang) {
+  document.querySelector('.sitename').textContent =
+    translations[lang].websiteTitle
+  document.getElementById('website-title').textContent =
+    translations[lang].websiteTitle
+  document.getElementById('heroDescription').textContent =
+    translations[lang].heroDescription
+
+  // Mettre à jour les éléments de navigation
+  document.querySelector('#navHome').textContent = translations[lang].menu.home
+  document.querySelector('#navProducts').textContent =
+    translations[lang].menu.products
+  document.querySelector('#navAbout').textContent =
+    translations[lang].menu.about
+  document.querySelector('#navContact').textContent =
+    translations[lang].menu.contact
+
+  document.querySelector('#products-title').textContent =
+    translations[lang].productSection.title
+  document.querySelector('#products-description').textContent =
+    translations[lang].productSection.description
+
+  document.querySelector('#all').textContent =
+    translations[lang].productSection.categories.all
+  document.querySelector('#pollinisateurs').textContent =
+    translations[lang].productSection.categories.cat1
+  document.querySelector('#equip').textContent =
+    translations[lang].productSection.categories.cat2
+
+  // products descriptions
+  document.querySelector('#prod1-desc').textContent =
+    translations[lang].productSection.products.prod1
+  document.querySelector('#prod2-desc').textContent =
+    translations[lang].productSection.products.prod2
+  document.querySelector('#prod3-desc').textContent =
+    translations[lang].productSection.products.prod3
+  document.querySelector('#prod4-desc').textContent =
+    translations[lang].productSection.products.prod4
+  document.querySelector('#prod5-desc').textContent =
+    translations[lang].productSection.products.prod5
+  document.querySelector('#prod6-desc').textContent =
+    translations[lang].productSection.products.prod6
+  document.querySelector('#prod7-desc').textContent =
+    translations[lang].productSection.products.prod7
+  document.querySelector('#prod8-desc').textContent =
+    translations[lang].productSection.products.prod8
+  document.querySelector('#prod9-desc').textContent =
+    translations[lang].productSection.products.prod9
+
+  document.querySelector('#about-title').textContent =
+    translations[lang].aboutSection.title
+  document.querySelector('#about-subtitle').textContent =
+    translations[lang].aboutSection.subtitle
+  document.querySelector('#about-description').textContent =
+    translations[lang].aboutSection.description
+
+  document.querySelector('#contact-title').textContent =
+    translations[lang].contactSection.title
+  document.querySelector('#contact-description').textContent =
+    translations[lang].contactSection.description
+
+  document.querySelector('#adress-title').textContent =
+    translations[lang].contactSection.adressTitle
+  document.querySelector('#adress-text').textContent =
+    translations[lang].contactSection.adressText
+  document.querySelector('#tel-title').textContent =
+    translations[lang].contactSection.telTitle
+  document.querySelector('#email-title').textContent =
+    translations[lang].contactSection.emailtitle
+
+  document.getElementById('footer-title').textContent =
+    translations[lang].footer.footerTitle
+  document.getElementById('footer-description').textContent =
+    translations[lang].footer.footerDesc
+  document.getElementById('footer-text').textContent =
+    translations[lang].footer.footerText
+}
